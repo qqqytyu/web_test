52078fefc07c4b42aad314fa26fc3d91d8729ae4
qqqytyu <qqqytyu@yeah.net>
2016-09-12 16:22:59 +0800

    16.9.12.16-22

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 
@@ -7,4 +7,7 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 # Create your views here.
