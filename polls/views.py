96f6a7201ee29a57a2606b9b446c09333003b390
qqqytyu <qqqytyu@yeah.net>
2016-09-12 16:57:23 +0800

    16.9.12.16-57

--- polls/views.py
+++ polls/views.py
@@ -6,8 +6,17 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([p.question_text for p in latest_question_list])
+    #return HttpResponse(output)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    pass
+
+def vote(request, question_id):
+    pass
 # Create your views here.
